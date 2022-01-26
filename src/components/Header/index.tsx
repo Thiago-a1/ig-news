946e425c932738405cfc0ba03dfe902175251917
Thiago Araujo <thiago.araujo.r11@gmail.com>
2022-01-26 16:40:34 -0300

    second commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { SighInButton } from '../SignInButton';
+
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+
+        <nav>
+          <a className={styles.active}>Home</a>
+          <a>Posts</a>
+        </nav>
+
+        <SighInButton />
+      </div>
+    </header>
+  );
+}
