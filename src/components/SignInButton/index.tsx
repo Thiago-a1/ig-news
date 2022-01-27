740933c10331d2c1437af032a2436d9552c1640e
Thiago Araujo <thiago.araujo.r11@gmail.com>
2022-01-27 11:57:39 -0300

    add nextauth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,19 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { useSession, signIn, signOut } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SighInButton() {
-  const isUserLoggedIn = true;
+  const { data } = useSession();
 
-  return isUserLoggedIn ? (
+  console.log(data)
+
+  return data ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color='#04d361' />
       Thiago Araujo
@@ -19,6 +23,7 @@ export function SighInButton() {
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color='#eba417' />
       Sing in with github
